abb3ef78abae5ae87aef7ad4e4b3424457afe19a
amossinhof-cloud <amossinhof@gmail.com>
2025-12-24 07:55:41 +0200

    Create train.py

---
+++ train.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Dados de exemplo
+data = {
+    "media_5": [1.5, 2.1, 1.8, 2.5, 1.2],
+    "media_10": [1.6, 2.0, 1.9, 2.3, 1.3],
+    "ultimo": [1.2, 2.5, 1.1, 3.0, 1.0],
+    "baixo_10": [4, 2, 3, 1, 5],
+    "alvo": [0, 1, 0, 1, 0]
+}
+
+df = pd.DataFrame(data)
+
+X = df[["media_5", "media_10", "ultimo", "baixo_10"]]
+y = df["alvo"]
+
+model = RandomForestClassifier()
+model.fit(X, y)
+
+joblib.dump(model, "aviator_model.pkl")
+print("Modelo criado com sucesso!")
