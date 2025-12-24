c0929fccdafb16f84cb6d3c8c1828ce014503778
amossinhof-cloud <amossinhof@gmail.com>
2025-12-24 08:49:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+from flask import Flask, request, jsonify
 import os
 import joblib
 import pandas as pd
-from flask import Flask, request, jsonify
 
 # Se o arquivo aviator_model.pkl não existir, cria ele
 if not os.path.exists("aviator_model.pkl"):
-    import train  # Executa train.py e cria o modelo
+    import train  # cria aviator_model.pkl
 
 app = Flask(__name__)
 model = joblib.load("aviator_model.pkl")
@@ -17,17 +17,14 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-
     entrada = pd.DataFrame([{
         "media_5": data["media_5"],
         "media_10": data["media_10"],
         "ultimo": data["ultimo"],
         "baixo_10": data["baixo_10"]
     }])
-
     prob = model.predict_proba(entrada)[0][1]
     sinal = "ENTRAR" if prob >= 0.65 else "NÃO ENTRAR"
-
     return jsonify({
         "sinal": sinal,
         "probabilidade": round(float(prob), 2),
